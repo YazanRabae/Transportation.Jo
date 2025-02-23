8966733f2f2391d254a9b03abbad1ddcbcce5677
Yazan <yazanrabae78@gmail.com>
2025-02-23 19:22:18 +0300

    Add project files.

---
+++ Transportation.Jo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Transportation.Jo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
